29b10d77b868d7151a48fb171d6ea128493079dd
whb-bigdata <whb_bigdata@163.com>
2020-12-03 09:04:59 +0800

    third

--- gmall0317-logger/src/main/java/com/atguigu/gmall0317logger/controller/Test.java
+++ gmall0317-logger/src/main/java/com/atguigu/gmall0317logger/controller/Test.java
@@ -3,5 +3,6 @@ package com.atguigu.gmall0317logger.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("third");
+        System.out.println("master");
     }
 }
