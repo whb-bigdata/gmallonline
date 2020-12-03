c9fc5ab5a979f4fa6e6b96278a4a0152f606d0b4
whb-bigdata <whb_bigdata@163.com>
2020-12-03 09:11:18 +0800

    分支2添加

---
+++ gmall0317-logger/src/main/java/com/atguigu/gmall0317logger/controller/Test2.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall0317logger.controller;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("third");
+        System.out.println("分支2");
+        System.out.println("分支2");
+        System.out.println("分支2");
+        System.out.println("分支2");
+
+    }
+}
