c331bff4ba6bcba9884648a25bf5a0aa45022743
whb-bigdata <whb_bigdata@163.com>
2020-12-09 23:36:48 +0800

    master更改

--- gmall0317-logger/src/main/java/com/atguigu/gmall0317logger/controller/Test2.java
+++ gmall0317-logger/src/main/java/com/atguigu/gmall0317logger/controller/Test2.java
@@ -2,7 +2,7 @@ package com.atguigu.gmall0317logger.controller;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("third");
+        System.out.println("master");
         System.out.println("master");
         System.out.println("master");
         System.out.println("master");
